f803766189191b2607a2f97b198bff4d9626fb23
BrianSolehin <ahmad.alden.sby@gmail.com>
2025-03-29 16:24:35 +0700

    feat: tambah fungsi logout

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
 "def login(): pass" 
 "def login(username, password):\n    return True" 
 "def validate_input(username):\n    return len(username) > 3" 
+"def logout():\n    return 'Logout berhasil'" 
